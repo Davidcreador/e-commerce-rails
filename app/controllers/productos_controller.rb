720d25a4d09f7e0c3df9febc4ccb6bd8e01acb6b
Helmut Pastor <hpastor@lacreativeria.com>
2017-04-27 22:16:57 -0600

    adding edit and update producto

--- app/controllers/productos_controller.rb
+++ app/controllers/productos_controller.rb
@@ -9,4 +9,23 @@ class ProductosController < ApplicationController
   def index
     @productos = Producto.all
   end
+
+  def edit
+    @producto = Producto.find(params[:id])
+  end
+
+  def update
+    @producto = Producto.find(params[:id])
+    if @producto.update_attributes(producto_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def producto_params
+      params.require(:producto).permit(:title, :cantidad, :content)
+    end
 end
